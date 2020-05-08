f967ecce0fb630e876f05b2d46de1a7babdd2aff
Barmasan <veselovskij.fedor@gmail.com>
2020-05-08 22:07:21 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
+const BOT_TOKEN = '1217779769:AAHazzIRAgxRt7FWyFUZUXnXePV_A3q0T38';
 
-const bot = new Telegraf(process.env.1217779769:AAHazzIRAgxRt7FWyFUZUXnXePV_A3q0T38)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply(''))
